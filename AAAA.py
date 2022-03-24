a7fa42d34de6242c1032acba3a3d72b70dd58123
Stella Ribli <18219027@std.stei.itb.ac.id>
2022-03-25 01:31:31 +0700

    finaaalll

--- AAAA.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-input_string = "U3Vubnk="
-base64_converted_string = input_string.encode("ascii")
-decode = base64.b64decode(base64_converted_string)
-decimal_converted_string = decode.decode("ascii")
-
-print(decimal_converted_string)
